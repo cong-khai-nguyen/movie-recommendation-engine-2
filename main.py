a253c332db1ce96f50ffb085a27bb8fb845c87b9
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-05 14:55:42 -0500

    standardize data by bringing the data mean to zero

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 df = pd.read_csv("data/toy_dataset.csv", index_col=0)
 print(df, "\n")
+def standardize(row):
+    new_row = (row - row.mean())/(row.max() - row.min())
+    return new_row
+
+ratings_std = df.apply(standardize)
+print(ratings_std, "\n")
