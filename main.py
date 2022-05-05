18ce5f93e3f8f96b9522fafa0356c9dbcccb11c1
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-05 15:02:13 -0500

    fill ratings NaN with 0 to apply cosine_similarity

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ def standardize(row):
 
 ratings_std = df.apply(standardize)
 print(ratings_std, "\n")
+
+ratings_std = ratings_std.fillna(0)
+print(ratings_std, "\n")
+
+# We are taking a transpose since we want similarity between items which need to be in rows
+similar_movie = cosine_similarity(ratings_std.T)
+print(similar_movie)
