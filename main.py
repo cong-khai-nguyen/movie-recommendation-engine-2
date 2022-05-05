c200c61abcb2b42013f6ffa879a0c5e238497858
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-05 14:51:46 -0500

    read in data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import math
 import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 
+df = pd.read_csv("data/toy_dataset.csv", index_col=0)
+print(df, "\n")
