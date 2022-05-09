5333ef578eb5935b7b43482599d464ba5a31eaae
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-09 12:36:12 -0500

    test the with a larger data set

--- main.py
+++ main.py
@@ -19,34 +19,35 @@ print(ratings_std, "\n")
 
 # ratings_std = ratings_std.fillna(0)
 # print(ratings_std, "\n")
+# After, fill the NaN with 0. We do this after because we don't want the model to think that user has rated the movie with 0
 ratings_std = np.nan_to_num(ratings_std)
 
 # We are taking a transpose since we want similarity between items which need to be in rows
 similar_movie = cosine_similarity(ratings_std.T)
 print(similar_movie)
 
-similar_movie_df = pd.DataFrame(similar_movie, index=df.columns, columns=df.columns)
-# similar_movie = pd.DataFrame(similar_movie, index=ratings_std.columns, columns=ratings_std.columns)
-print(similar_movie_df)
-
-def get_similar_movies(movie_name, user_rating):
-    # We take user_rating subtracting 2.5 because 2.5 is a mean a for a rating. That would help us to push down those related movie to that bad rating toward the bottom
-    similar_score = similar_movie_df[movie_name] * (user_rating - 2.5)
-    similar_score = similar_score.sort_values(ascending = False)
-    return similar_score
-
-print(get_similar_movies("action1",2))
-
-# However, in real life, we are always given a list of ratings from user
-movies_ratings = [("action1", 5), ("romantic2", 5), ("romantic3", 1)]
-
-related_movies = pd.DataFrame()
-print("\n\n")
-for movie, ratings in movies_ratings:
-    related_movies = related_movies.append(get_similar_movies(movie, ratings), ignore_index=True)
-
-print(related_movies.head())
-print(related_movies.sum().sort_values(ascending=False))
+# similar_movie_df = pd.DataFrame(similar_movie, index=df.columns, columns=df.columns)
+# # similar_movie = pd.DataFrame(similar_movie, index=ratings_std.columns, columns=ratings_std.columns)
+# print(similar_movie_df)
+#
+# def get_similar_movies(movie_name, user_rating):
+#     # We take user_rating subtracting 2.5 because 2.5 is a mean a for a rating. That would help us to push down those related movie to that bad rating toward the bottom
+#     similar_score = similar_movie_df[movie_name] * (user_rating - 2.5)
+#     similar_score = similar_score.sort_values(ascending = False)
+#     return similar_score
+#
+# # print(get_similar_movies("action1",2))
+#
+# # However, in real life, we are always given a list of ratings from user
+# movies_ratings = [("action1", 5), ("romantic2", 5), ("romantic3", 1)]
+#
+# related_movies = pd.DataFrame()
+# print("\n\n")
+# for movie, ratings in movies_ratings:
+#     related_movies = related_movies.append(get_similar_movies(movie, ratings), ignore_index=True)
+#
+# print(related_movies.head())
+# print(related_movies.sum().sort_values(ascending=False))
 
 
 # Now I want to test my recommendation algorithm with real life data sets
@@ -65,16 +66,15 @@ user_ratings = movies_ratings.pivot_table(index = ['userId'], columns = ['title'
 print(user_ratings.head(2))
 
 # Remove movies which have less than 3 ratings to remove irrelevancy due to not having enough data acquired
-user_ratings = user_ratings.dropna(thresh = 3, axis = 1)
+user_ratings = user_ratings.dropna(thresh = 10, axis = 1)
 # print(user_ratings.head(2))
 # user_ratings_std = std.fit_transform(user_ratings)
 
 # Get pearson correlation between data
-# After, fill the NaN with 0. We do this after because we don't want the model to think that user has rated the movie with 0
 # Since applying pearson method already adjust the mean, I don't have to scale the data
-similar_movie_df = user_ratings.corr(method="pearson").fillna(0)
+similar_movie_df = user_ratings.fillna(0).corr(method="pearson")
 print(similar_movie_df)
-
+related_movies = pd.DataFrame()
 # Recreate the helper method to find the similar movies
 def get_similar_movies(movie_name, user_rating):
     # We take user_rating subtracting 2.5 because 2.5 is a mean a for a rating. That would help us to push down those related movie to that bad rating toward the bottom
@@ -87,5 +87,5 @@ movies_ratings = [("2 Fast 2 Furious (Fast and the Furious 2, The) (2003)", 5), 
 for movie, ratings in movies_ratings:
     related_movies = related_movies.append(get_similar_movies(movie, ratings), ignore_index=True)
 
-print(related_movies.head())
-print(related_movies.sum().sort_values(ascending=False))
+# print(related_movies.head())
+print(related_movies.sum().sort_values(ascending=False).head(20))
